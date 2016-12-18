4de3d35d5328f261f7f3268ac09dcd530b14647a
Paul Beaudet <inof8or@gmail.com>
2016-12-17 20:32:28 -0500

    fix post validation

--- paymentNotificationServer.js
+++ paymentNotificationServer.js
@@ -1,9 +1,6 @@
 // paymentNotificationServer.js ~ Copyright 2016 Mancehster Makerspace ~ MIT License
 var slack = require('./our_modules/slack_intergration.js');// import our slack module
 var crypto = require('./our_modules/crypto.js');           // abstracted message scrambling
-var URI = 'https://www.paypal.com/cgi-bin/webscr';
-if(process.env.TESTING_STATE === 'true'){URI = 'https://www.sandbox.paypal.com/cgi-bin/webscr';}
-console.log('listending for ' + URI); // just to be sure we know in the log what we intended to be listening to
 
 var sockets = {                                            // instantiate socket server
     server: require('socket.io'),                          // grab socket.io library
@@ -40,50 +37,47 @@ var sockets = {                                            // instantiate socket
 var paypal = {
     request: require('request'),
     querystring: require('querystring'),
-    options: function (postreq){
+    options: function (postreq, responseURI){
         return {
-            uri: URI,
-            method: 'POST',
-            headers:{'Connection': 'close'},
-            body: postreq,
-            strictSSL: true,
-            rejectUnauhtorized: false,
-            requestCert: true,
-            agent: false
+            uri: responseURI, method: 'POST', headers:{'Connection': 'close'},
+            body: postreq, strictSSL: true, rejectUnauhtorized: false,
+            requestCert: true, agent: false
         };
     },
-    listenEvent: function(req, res){
-        if(req.body && req.body.reciever_email === process.env.PAYPAL_EMAIL){  // verify this payment is comming from a payment to our email
-            res.status(200).send('OK');                                        // Step 1 ACK notification
-            res.end();                                                         // end response
-            paypal.ack(req.body);                                              // ackknowlage post w/ return post
-        }
-    },
-    ack: function(oBody){
-        var postreq = 'cmd=_notify-validate'; // step 2 read ipn message and prepend with _notify-validate and post back to paypal
-        for(var key in oBody){             // not quite sure that this is right
-            if(oBody.hasOwnProperty(key)){ // for all keys
-                postreq = postreq + '&' + key + '=' + paypal.querystring.escape(oBody[key]); // build new post body
+    listenEvent: function(responseURI){                                    // create route handler for test or prod
+        return function(req, res){                                         // route handler
+            if(req.body){                                                  // verify payment is comming from a payment to our email
+                res.status(200).send('OK');                                // ACK notification
+                res.end();                                                 // end response
+                if(req.body.reciever_email === process.env.PAYPAL_EMAIL){  // make sure we are meant to recieve this payment
+                    var postreq = 'cmd=_notify-validate';    // read ipn message and prepend with _notify-validate and post back to paypal
+                    for(var key in req.body){                // not quite sure that this is right its from an example
+                        if(req.body.hasOwnProperty(key)){    // for all keys
+                            postreq = postreq + '&' + key + '=' + paypal.querystring.escape(req.body[key]); // build new post body
+                        }
+                    }    // Prove they sent what they think they sent you, post it back to them
+                    paypal.request(paypal.options(postreq, responseURI), paypal.requestResponse(req.body));
+                } else { // log any funny business
+                    console.log('reciver email:' + req.body.reciever_email + ' is not equel to ' + process.env.PAYPAL_EMAIL);
+                }
             }
-        } // Prove they sent what they think they sent you, post it back to them
-        paypal.request(paypal.options(postreq), paypal.requestResponse(oBody));
+        };
     },
     requestResponse: function(oBody){
         return function(error, response, body){
-            if(error){
-                slack.sendAndLog('IPN response issue:' + error);
-            } else if(response.statusCode === 200){
-                if(body.substring(0, 8) === 'VERIFIED' && oBody.payment_status === 'complete'){
-                    var itemName = 'item_name:' + oBody.itemName + ' or item_name1:' + oBody.item_name1;
-                    console.log(JSON.stringify(oBody));   // get an idea of data we are dealing with
+            console.log('response body:'+ JSON.stringify(body));
+            if(error){slack.sendAndLog('IPN response issue:' + error);}
+            else if(response.statusCode === 200){
+                console.log(JSON.stringify(oBody));       // get an idea of data we are dealing with
+                if(body.substring(0, 8) === 'VERIFIED'){
                     // send oBody.txn_id to note transaction number, if number is same as an old one its invalid
-                    slack.send('$'+ oBody.mc_gross +' pament for '+ itemName +' from '+ oBody.first_name +' '+ oBody.last_name);
+                    if(oBody.payment_status === 'Completed'){ // varify that this is a completed payment
+                        slack.send('$'+ oBody.mc_gross + ' pament for '+ oBody.itemName+' from '+ oBody.first_name +' '+ oBody.last_name);
+                    } // send to renewal channel who just paid!
                 } else if (body.substring(0, 7) === 'INVALID') {
                     slack.sendAndLog('Invalid IPN POST'); // IPN invalid, log for manual investigation
                 }
-            } else {
-                slack.sendAndLog('IPN post, other code: ' + response.statusCode);
-            }
+            } else {slack.sendAndLog('IPN post, other code: ' + response.statusCode);}
         };
     }
 };
@@ -94,12 +88,13 @@ var serve = {                                                // depends on cooki
     theSite: function (){                                    // methode call to serve site
         var app = serve.express();                           // create famework object
         var http = require('http').Server(app);              // http server for express frameworkauth)
-        // app.use(serve.parse.json());                         // support JSON-encoded bodies
         app.use(serve.parse.urlencoded({extended: true}));   // support URL-encoded bodies
         app.use(serve.express.static(__dirname + '/views')); // serve page dependancies (sockets, jquery, bootstrap)
         var router = serve.express.Router();                 // create express router object to add routing events to
         router.get('/', function(req, res){res.send('Everything is going to be ok kid, Everythings going to be OKAY');});
-        router.post('/ipnlistener', paypal.listenEvent);     // request registration page
+        router.post('/ipnlistener', paypal.listenEvent('https://www.paypal.com/cgi-bin/webscr'));   // real listener post route
+        router.post('/sand', paypal.listenEvent('https://www.sandbox.paypal.com/cgi-bin/webscr'));  // test with paypal's IPN simulator
+        router.post('/test', paypal.listenEvent('http://localhost:8378/test'));                     // test w/ a local IPN simulator
         app.use(router);                                     // get express to user the routes we set
         return http;
     }
@@ -110,5 +105,5 @@ sockets.listen(http);                                        // listen and handl
 http.listen(process.env.PORT);                               // listen on specified PORT enviornment variable
 // intilize slack bot to talk to x channel, with what channel it might use
 if(slack.init(process.env.SLACK_WEBHOOK_URL, process.env.BROADCAST_CHANNEL, process.env.SLACK_TOKEN)){
-    slack.send('IPN Restart, testing= ' + process.env.TESTING_STATE);
-}
+    slack.send('Payment listener woke up');
+} else {console.log('failed to connect to slack!');}
