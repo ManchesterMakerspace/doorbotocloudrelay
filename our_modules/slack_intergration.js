447b537630bac2f69490c6d53097d7e038b1b557
Paul Beaudet <inof8or@gmail.com>
2017-01-09 20:05:59 -0500

    give this thing a new name and look, it is really something different

--- our_modules/slack_intergration.js
+++ our_modules/slack_intergration.js
@@ -7,9 +7,9 @@ var slack = {
         try {                                          // slack is not a dependancy, will fail softly if no internet or slack
             slack.token = token;                       // authentication to post as and invidual (in this case an admin user is needed to inivite new members)
             slack.wh = new slack.webhook(webhookURL, { // instantiate webhook (bot) w/ its url and profile
-                username: 'doorboto',                  // Name of bot
+                username: 'Payment Listener',          // Name of bot
                 channel: channelToSentTo,              // channel that this intergration spams in particular
-                iconEmoji: ':robot_face:',             // icon emoji that bot uses for a profile picture
+                iconEmoji: ':moneybag:',               // icon emoji that bot uses for a profile picture
             });
             return true;                               // note succesful start up
         } catch(error){                                // handle not being connected
